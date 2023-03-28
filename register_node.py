0dd31a0641964da6df5da1d55eac19a332d9cb01
Shon Lev-Ran <shonl@directeam.io>
2023-03-28 23:39:39 +0300

    Improve the code

--- register_node.py
+++ register_node.py
@@ -1,18 +1,13 @@
-# register_node.py
-
 import os
-import json
-import boto3
 import requests
 from requests.exceptions import RequestException
-import pyshark
-import queue
-import threading
 import time
-from prometheus_client import start_http_server, Counter
 
 METADATA_BASE_URL = "http://169.254.169.254/latest/"
 METADATA_TIMEOUT = 1.0
+
+class MetadataError(Exception):
+    pass
 
 def get_imdsv2_token():
     try:
@@ -22,8 +17,7 @@ def get_imdsv2_token():
         response.raise_for_status()
         return response.text
     except RequestException as e:
-        print(f"Error fetching IMDSv2 token: {e}")
-        return None
+        raise MetadataError(f"Error fetching IMDSv2 token: {e}")
 
 def get_metadata(path, token):
     try:
@@ -31,39 +25,41 @@ def get_metadata(path, token):
         response.raise_for_status()
         return response.text
     except RequestException as e:
-        print(f"Error fetching metadata: {e}")
-        return None
+        raise MetadataError(f"Error fetching metadata: {e}")
 
 def register_node():
     token = get_imdsv2_token()
     if not token:
-        print("Failed to fetch IMDSv2 token. Exiting.")
-        return
+        raise MetadataError("Failed to fetch IMDSv2 token. Exiting.")
 
     node_name = get_metadata("hostname", token)
     node_ip = get_metadata("local-ipv4", token)
     availability_zone = get_metadata("placement/availability-zone", token)
     availability_zone_id = get_metadata("placement/availability-zone-id", token)
 
-    if node_name and node_ip and availability_zone:
-        return {
-            "node_name": node_name,
-            "node_ip": node_ip, 
-            "availability_zone": availability_zone,
-            "availability_zone_id": availability_zone_id
-        }
+    if not (node_name and node_ip and availability_zone):
+        raise MetadataError("Failed to fetch metadata. Exiting.")
 
-    else:
-        print("Failed to fetch metadata. Exiting.")
+    return {
+        "node_name": node_name,
+        "node_ip": node_ip, 
+        "availability_zone": availability_zone,
+        "availability_zone_id": availability_zone_id
+    }
 
-node_metadata = register_node()
-if not node_metadata:
-    print("Failed to fetch node_metadata. Exiting.")
-    pass
+try:
+    node_metadata = register_node()
+except MetadataError as e:
+    print(str(e))
+    exit(1)
 
 # Keep the main thread alive
+def shutdown_hook():
+    print("Shutting down...")
+    # do cleanup here
+
 try:
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
-    pass
+    shutdown_hook()
